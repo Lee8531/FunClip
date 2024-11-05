3c5cdd14a96bad009882b984c6dc94cedb81eaa3
李继恩 <lijien@benlai.com>
2024-11-05 21:31:05 +0800

    去掉音频和默认模型

--- funclip/launch.py
+++ funclip/launch.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='argparse testing')
     parser.add_argument('--lang', '-l', type=str, default = "zh", help="language")
     parser.add_argument('--share', '-s', action='store_true', help="if to establish gradio share link")
-    parser.add_argument('--port', '-p', type=int, default=7860, help='port number')
+    parser.add_argument('--port', '-p', type=int, default=6006, help='port number')
     parser.add_argument('--listen', action='store_true', help="if to listen to all hosts")
     args = parser.parse_args()
     
